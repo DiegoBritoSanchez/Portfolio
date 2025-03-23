13a407b3420659a63abd3360849f19c91941d067
Diego Brito Sánchez <diegobritosanchez13@gmail.com>
2025-03-23 01:55:51 +0000

    Fix

--- config/urls.py
+++ config/urls.py
@@ -2,12 +2,21 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/profile/", include("core.urls")),  # Incluimos las rutas de core
-    path("api/projects/", include("projects.urls")),  # Incluimos las rutas de projects
+    path("api/profile/", include("core.urls")),      # Incluimos las rutas de core
+    path("api/projects/", include("projects.urls")), # Incluimos las rutas de projects
 ]
 
+# Servir archivos media en desarrollo
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Servir archivos media en producción
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    ]
